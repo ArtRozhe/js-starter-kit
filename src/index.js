09eb67ec25eec6c4d6aea98cb65407d412ffc602
rav <rav@em70.ru>
2018-08-04 20:33:58 +0700

    added API mocking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-import numeral from 'numeral';
-
 import './index.css';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome starter-kit!`); // eslint-disable-line no-console
+import { getUsers, deleteUser } from './api/userApi';
+
+getUsers().then(result => {
+   let usersBody = '';
+
+   result.forEach(user => {
+      usersBody += `<tr><td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td><td>${user.id}</td><td>${user.name}</td>`;
+   });
+
+   global.document.getElementById('users').innerHTML = usersBody;
+
+   const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+   // Must use array.from to create a real array from a DOM collection
+   // getElementByClassName only returns an "array like" object
+   Array.from(deleteLinks, link => {
+       link.onclick = function(event) {
+           event.preventDefault();
+           const element = event.target;
+           deleteUser(element.attributes['data-id'].value);
+           const row = element.parentNode.parentNode;
+           row.parentNode.removeChild(row);
+       }
+   })
+});
