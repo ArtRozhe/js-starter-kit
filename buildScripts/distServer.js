78d70f5b8b292d5b303ac550ce9af60476e77f29
rav <rav@em70.ru>
2018-08-05 12:44:06 +0700

    moved API to heroku

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,15 +15,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function(req, res) {
-    // Hard coding for simplicity. Pretend this hits a real database
-    res.json([
-        {"id": 1,"name":"Bob"},
-        {"id": 2,"name":"Tammy"},
-        {"id": 3,"name":"Tina"}
-    ]);
-});
-
 app.listen(port, function (err) {
     if (err) {
         console.log(err);
